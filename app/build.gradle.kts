7878203e9cfd3b55501fb8c7e58eb46aa78f0625
Malik05255 <mmbbrr99@gmail.com>
2026-09-14 21:37:33 +0300

    Target Android API 37.0 platform explicitly

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,11 @@ plugins {
 
 android {
     namespace = "com.hai.manager"
-    compileSdk = 37
+    compileSdk {
+        version = release(37) {
+            minorApiLevel = 0
+        }
+    }
 
     defaultConfig {
         applicationId = "com.hai.manager"
