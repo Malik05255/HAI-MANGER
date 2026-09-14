c5f65a81480eeebfd0935833cb2331f5040395eb
Malik05255 <mmbbrr99@gmail.com>
2026-09-15 00:33:25 +0300

    Bump HAI MANAGER to 0.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.hai.manager"
         minSdk = 26
         targetSdk = 37
-        versionCode = 12
-        versionName = "0.8.0"
+        versionCode = 13
+        versionName = "0.9.0"
     }
 
     val releaseKeystore = System.getenv("HAI_KEYSTORE_PATH")
